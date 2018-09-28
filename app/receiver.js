aeb4da8d3e61e8c91f68b0f4363996c7bea87da2
GBagretsov <gbagretsov@gmail.com>
2018-09-28 21:58:28 +0300

    Добавлен ответ бота на простые сообщения

--- app/receiver.js
+++ app/receiver.js
@@ -1,4 +1,9 @@
 const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const axios = require('axios');
+const accessToken = process.env.ACCESS_TOKEN;
+const peerID = process.env.PEER_ID;
 
 module.exports = function(app){
 
@@ -12,7 +17,28 @@ module.exports = function(app){
       return;
     } 
     
-    // TODO: приём сообщений
+    // Приём сообщений
+    if (req.body.type === 'message_new') {
+      if (peerID == req.body.object.peer_id) {
+        console.log(req.body.object);
+        let msg = req.body.object.text;
+
+        // TODO: логика игры
+        if (msg.startsWith('Бот,') || msg.includes('club171869330')) {
+          let uid = req.body.object.from_id;
+          axios.get(`https://api.vk.com/method/users.get?v=5.85&access_token=${accessToken}&user_ids=${uid}`)
+            .then(function (response) {
+              require('./sender').sendMessage(`Привет, ${ response.data.response[0].first_name }!`);
+            });
+          } else {
+            console.log('I didn\'t understand this message :(');
+          }
+      } else {
+        console.log('This message is not for me');
+      };
+      res.send('ok');
+      return;
+    }
 
     res.status(404).end();
   });
