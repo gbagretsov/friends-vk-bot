206b3204c8751915f7cdfcb72aec0858e7bcfda7
GBagretsov <gbagretsov@gmail.com>
2018-10-16 02:23:13 +0300

    Добавлен Material UI

--- front/App.jsx
+++ front/App.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import Button from '@material-ui/core/Button';
 import './App.scss';
 
 class App extends Component{
@@ -7,6 +10,7 @@ class App extends Component{
       <div className="App">
         <h1>Hello from bot!</h1>
         <p>TODO: страница администратора</p>
+        <Button variant="contained" color="primary">Кнопка</Button>
       </div>
     );
   }
