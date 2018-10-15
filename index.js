de6e336e75b42be10baf674c6d41104a680026d7
GBagretsov <gbagretsov@gmail.com>
2018-10-15 16:11:24 +0300

    Добавлена поддержка React

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => res.render('pages/index'))
-
 require('./app/receiver')(app)
 
 app.use('/words', require('./app/game/admin').router);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './front/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'public/dist/'),
+    filename: 'bundle.js'
+  },
+};
