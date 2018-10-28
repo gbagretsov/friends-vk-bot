f953d5629ce15ea3834b5971fbcead1d5879d7d0
GBagretsov <gbagretsov@gmail.com>
2018-10-28 20:48:58 +0300

    Добавлена возможность добавлять слова

--- app/api/words.js
+++ app/api/words.js
@@ -18,6 +18,26 @@ router.get('/', (req, res) => {
     .then(() => client.end());
 });
 
+router.post('/', (req, res) => {
+
+  const client = db();
+
+  let name = req.body.name;
+  // TODO: валидация
+
+  client.query(`INSERT INTO friends_vk_bot.words (name) VALUES ('${ name }') RETURNING id;`)
+    .then(r => {
+      let id = r.rows[0].id;
+      let word = { id, name };
+      res.json({ success: true, word });
+    })
+    .catch(error => {
+      console.log(error);
+      res.json({ error: error.code == 23505 ? 'duplicate' : 'internal' });
+    })
+    .then(() => client.end());
+});
+
 router.post('/:id', (req, res) => {
 
   const client = db();
--- front/words/Words.jsx
+++ front/words/Words.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
 
 import Word from './Word.jsx';
 
@@ -14,6 +17,7 @@ class Words extends Component{
     super(props);
     this.state = {
       words: [],
+      newWordInputValue: '',
     };
   }
 
@@ -61,8 +65,68 @@ class Words extends Component{
     this.props.onWordDeleted(deleted);
   }
 
+  addWord = () => {
+    let name = this.state.newWordInputValue;
+    // TODO: валидация
+
+    let url = `api/words`;
+
+    let options = {
+      token: this.props.token,
+      name,
+    }
+
+    let params = {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(options),
+    }
+
+    fetch(url, params)
+    .then(response => response.json())
+    .then(data => {
+      if (data.error) {
+        throw new Error(data.error);
+      } else {
+        let word = data.word;
+        this.setState(state => {
+          let words = state.words;
+          words.unshift(word);
+          return { words };
+        });
+        this.props.onWordAdded(word);
+      }
+    })
+    .catch(error => {
+      if (error.message === 'duplicate') {
+        var errorMessage = `Слово "${name}" уже добавлено`;
+      } else {
+        var errorMessage = 'Произошла ошибка, попробуйте позднее';
+      }
+      this.props.onError(errorMessage);
+    })
+    .then(() => this.setState({
+      newWordInputValue: '',
+    }));
+  }
+
+  handleNewWordChange = (event) => {
+    this.setState({
+      newWordInputValue: event.target.value,
+    });
+  }
+
+  _handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.addWord();
+    }
+  }
+
   render(){
-    let { words } = this.state;
+    let { words, newWordInputValue } = this.state;
     let wordsElements = words.map(word =>
       <Word
         key={word.id}
@@ -80,6 +144,17 @@ class Words extends Component{
         <Paper className="paper">
           <Typography variant="h5" gutterBottom>Список слов</Typography>
           <ul>
+            <li>
+              <div>
+                <TextField
+                  value={newWordInputValue}
+                  onChange={this.handleNewWordChange}
+                  onKeyPress={this._handleKeyPress}
+                  placeholder="Новое слово"
+                />
+                <IconButton className="button" onClick={ () => this.addWord() }><AddIcon fontSize="small"/></IconButton>
+              </div>
+            </li>
             { wordsElements }
           </ul>
         </Paper>
