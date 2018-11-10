18fb471240e5d3e7861e2e072b86a1dc105d5aa6
GBagretsov <gbagretsov@gmail.com>
2018-11-10 21:14:10 +0300

    Добавлено распознавание текста

--- app/speech/speech.js
+++ app/speech/speech.js
@@ -1,8 +1,52 @@
+require('dotenv').config();
+const needle = require('needle');
+const Audio = require('audio');
+const fs = require('fs');
+
+let handleAudioMessage = async (audioMessage) => {
+  // Получаем аудиозапись
+  let rawAudio = await needle('get', audioMessage.link_mp3);
+  rawAudio = rawAudio.body;
+  
+  // TODO: сделать без сохранения на диск
+
+  // Конвертируем в нужный формат
+  fs.writeFileSync(__dirname + '/audio.mp3', rawAudio);
+  Audio.cache = {};
+  Audio.load('./audio.mp3').then(audio => audio.save('audio.wav', onAudioSaved));
+};
+
+let onAudioSaved = async () => {
+  let rawAudio = fs.readFileSync(__dirname + '/audio.wav');
+
+  // Загружаем аудио на сервер Google
+  let url = `https://speech.googleapis.com/v1/speech:recognize?key=${process.env.GOOGLE_KEY}`;
+  let base64Audio = Buffer.from(rawAudio).toString('base64');
+  let data = {
+    config: {
+      languageCode: 'ru-RU',
+      profanityFilter: true,
+    },
+    audio: {
+      content: base64Audio,
+    },
+  };
+  let recognizedText = await needle('post', url, data, { json: true });
+
+  try {
+    console.log(recognizedText.body.results[0].alternatives[0]);
+    console.log(recognizedText.body.results[0].alternatives.length);
+  } catch (error) {
+    console.log('no result');
+  }
+
+  // TODO: отправлять ответ ВК
+};
+
 module.exports = function(message) {
   if (message.attachments && message.attachments[0] && message.attachments[0].type === 'audio_message') {
     let audioMessage = message.attachments[0].audio_message;
-    // TODO: обработка аудиосообщения
-    console.log(audioMessage);
+    handleAudioMessage(audioMessage);
     return true;
   }
   return false;
