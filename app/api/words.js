c6f2935cde18d4ffffcebc7b087b580464c1413f
GBagretsov <gbagretsov@gmail.com>
2019-05-12 14:15:48 +0300

    Добавлены async-функции в модуль api/words

--- app/api/words.js
+++ app/api/words.js
@@ -2,75 +2,77 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
 
   const client = db();
-
-  client.query('SELECT * FROM friends_vk_bot.words ORDER BY id DESC;')
-    .then(r => {
-      let words = r.rows;
-      res.json(words);
-    })
-    .catch(error => {
-      console.log(error);
-      res.json({ error: 'internal' });
-    })
-    .then(() => client.end());
+  
+  try {
+    const r = await client.query('SELECT * FROM friends_vk_bot.words ORDER BY id DESC;');
+    let words = r.rows;
+    res.json(words);
+  } catch(error) {
+    console.log(error);
+    res.json({ error: 'internal' });
+  } finally {
+    client.end();
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 
   const client = db();
 
   let name = req.body.name;
   // TODO: валидация
 
-  client.query(`INSERT INTO friends_vk_bot.words (name) VALUES ('${ name }') RETURNING id;`)
-    .then(r => {
-      let id = r.rows[0].id;
-      let word = { id, name };
-      res.json({ success: true, word });
-    })
-    .catch(error => {
-      console.log(error);
-      res.json({ error: error.code == 23505 ? 'duplicate' : 'internal' });
-    })
-    .then(() => client.end());
+  try {
+    const r = await client.query(`INSERT INTO friends_vk_bot.words (name) VALUES ('${ name }') RETURNING id;`);
+    let id = r.rows[0].id;
+    let word = { id, name };
+    res.json({ success: true, word });
+  } catch(error) {
+    console.log(error);
+    res.json({ error: error.code == 23505 ? 'duplicate' : 'internal' });
+  } finally {
+    client.end();
+  }
 });
 
-router.post('/:id', (req, res) => {
+router.post('/:id', async (req, res) => {
 
   const client = db();
 
   let id = req.params.id;
   let name = req.body.name;
 
-  client.query(`UPDATE friends_vk_bot.words SET name='${name}' WHERE id=${id};`)
-    .then(r => {
-      res.json({ success: true });
-    })
-    .catch(error => {
-      console.log(error);
-      res.json({ error: 'internal' });
-    })
-    .then(() => client.end());
+  try {
+    await client.query(`UPDATE friends_vk_bot.words SET name='${name}' WHERE id=${id};`);
+    res.json({ success: true });
+  } catch(error) {
+    console.log(error);
+    res.json({ error: 'internal' });
+  } finally {
+    client.end();
+  }
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
 
   const client = db();
 
   let id = req.params.id;
 
-  client.query(`DELETE FROM friends_vk_bot.words WHERE id=${id};`)
-    .then(r => {
-      res.json({ success: true });
-    })
-    .catch(error => {
-      console.log(error);
-      res.json({ error: 'internal' });
-    })
-    .then(() => client.end());
+  try {
+    await client.query(`DELETE FROM friends_vk_bot.words WHERE id=${id};`);
+    res.json({ success: true });
+  } catch(error) {
+    console.log(error);
+    res.json({ error: 'internal' });
+  } finally {
+    client.end();
+  }
+
 });
 
 module.exports.router = router;
