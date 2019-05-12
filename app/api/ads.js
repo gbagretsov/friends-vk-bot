7cfcb1d526d10747a27e844073643351eb104b2a
GBagretsov <gbagretsov@gmail.com>
2019-05-12 13:50:18 +0300

    Добавлены async-функции в модуль api/ads

--- app/api/ads.js
+++ app/api/ads.js
@@ -2,35 +2,37 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const client = db();
 
-  client.query("SELECT value FROM friends_vk_bot.state WHERE key = 'ads';")
-  .then(r => {
-    ads = r.rows[0].value;
+  try {
+    let r = await client.query("SELECT value FROM friends_vk_bot.state WHERE key = 'ads';");
+    let ads = r.rows[0].value;
     res.json({ ads });
-  })
-  .catch(error => {
+  } catch(error) {
     console.log(error);
     res.json({ error: 'internal' });
-  })
-  .then(() => client.end());
+  } finally {
+    client.end();
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   let newAds = req.body.ads;
   let query = `
     UPDATE friends_vk_bot.state SET value = '${ newAds }' WHERE key = 'ads';
   `;
 
   const client = db();
-  client.query(query)
-  .then(() => res.json({ success: true }))
-  .catch(error => {
+  try {
+    await client.query(query);
+    res.json({ success: true });
+  } catch(error) {
     console.log(error);
     res.json({ error: 'internal' });
-  })
-  .then(() => client.end());
+  } finally {
+    client.end();
+  }
 });
 
 module.exports.router = router;
