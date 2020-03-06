40ec969aef6887b3d44423b192448312bbd2da87
Georgii Bagretsov <georgii.bagretsov@t-systems.com>
2020-03-06 22:24:54 +0300

    Добавлены миграции БД

--- migrations/1_words-state-polls.sql
+++ migrations/1_words-state-polls.sql
@@ -1,14 +1,12 @@
-CREATE SCHEMA friends_vk_bot;
+-- Up migration
+
+CREATE SCHEMA IF NOT EXISTS friends_vk_bot;
 
 
 CREATE TABLE IF NOT EXISTS friends_vk_bot.words
 (
-	id SERIAL NOT NULL
-		CONSTRAINT actors_pkey
-			PRIMARY KEY,
-	name TEXT NOT NULL
-		CONSTRAINT unique_name
-			UNIQUE
+	id SERIAL NOT NULL PRIMARY KEY,
+	name TEXT NOT NULL UNIQUE
 );
 
 INSERT INTO friends_vk_bot.words (name) VALUES ('бот');
@@ -16,9 +14,7 @@ INSERT INTO friends_vk_bot.words (name) VALUES ('бот');
 
 CREATE TABLE IF NOT EXISTS friends_vk_bot.state
 (
-	key TEXT NOT NULL
-		CONSTRAINT state_pkey
-			PRIMARY KEY,
+	key TEXT NOT NULL PRIMARY KEY,
 	value TEXT
 );
 
@@ -29,10 +25,12 @@ INSERT INTO friends_vk_bot.state (key, value) VALUES ('answer', '');
 
 CREATE TABLE IF NOT EXISTS friends_vk_bot.polls
 (
-	id BIGINT NOT NULL
-		CONSTRAINT polls_pkey
-			PRIMARY KEY,
+	id BIGINT NOT NULL PRIMARY KEY,
 	owner_id BIGINT NOT NULL
 );
 
+
+-- Down migration
+
+DROP SCHEMA IF EXISTS friends_vk_bot CASCADE;
 
