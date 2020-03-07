f402614bae000f2ccf2f91bb31b1986a1154bc72
Georgii Bagretsov <georgii.bagretsov@t-systems.com>
2020-03-07 21:17:57 +0300

    Рефакторинг модуля game

--- app/game/game.js
+++ app/game/game.js
@@ -17,7 +17,7 @@ const STEP_INTERVAL = process.env.GAME_STEP_INTERVAL || 15000;
 let message, answer;
 let timeoutObj;
 
-function getRandomTask() {
+async function getRandomTask() {
   // TODO: поддержка категорий
   let tableName = TABLE_WORDS;
   let category = 'слово';
@@ -27,23 +27,17 @@ function getRandomTask() {
     ORDER BY RANDOM() LIMIT 1;
   `;
 
-  let toReturn;
   let client = dbClient();
 
-  return client.query(query)
-    .then(result => {
-      toReturn = {
-        category: category,
-        answer: result.rows[0].name,
-      };
-      return client.end();
-    })
-    .then(result => {
-      return toReturn;
-    });
+  const dbResult = await client.query(query);
+  await client.end();
+  return {
+    category: category,
+    answer: dbResult.rows[0].name,
+  };
 }
 
-function setGameState(state) {
+async function setGameState(state) {
   let query = `
     BEGIN TRANSACTION;
     UPDATE friends_vk_bot.state SET value = '${ state.state }' WHERE key = 'state';
@@ -51,38 +45,34 @@ function setGameState(state) {
     COMMIT TRANSACTION;
   `;
   let client = dbClient();
-  return client.query(query)
-    .then(() => client.end());
+  await client.query(query);
+  await client.end();
 }
 
-function getGameState() {
+async function getGameState() {
   let state = {};
   let client = dbClient();
-  return client.query('SELECT * FROM friends_vk_bot.state;')
-    .then(result => {
-      for (let row of result.rows) {
-        state[row['key']] = row['value'];
-      }
-      return client.end();
-    })
-    .then(() => state);
+  const dbResult = await client.query('SELECT * FROM friends_vk_bot.state;');
+  await client.end();
+  for (let row of dbResult.rows) {
+    state[row['key']] = row['value'];
+  }
+  return state;
 }
 
-function handleMessage(resolve, reject) {
-  getGameState()
-    .then(state => {
-      if (handleAddWordRequest() || handleDeleteWordRequest()) {
-        resolve(true);
-      } else if (state.state === STATE_IDLE) {
-        handleIdleState(resolve, reject);
-      } else if (state.state === STATE_PLAYING) {
-        answer = state.answer;
-        handlePlayingState(resolve, reject);
-      }
-    });
+async function handleMessage(resolve, reject) {
+  const state = await getGameState();
+  if (await handleAddWordRequest() || await handleDeleteWordRequest()) {
+    resolve(true);
+  } else if (state.state === STATE_IDLE) {
+    await handleIdleState(resolve, reject);
+  } else if (state.state === STATE_PLAYING) {
+    answer = state.answer;
+    await handlePlayingState(resolve, reject);
+  }
 }
 
-function handleAddWordRequest() {
+async function handleAddWordRequest() {
   let text = this.message.text.toLowerCase();
   let botMentioned = isBotMentioned(text);
   let isAddWordRequest = text.includes('запомни слово');
@@ -91,14 +81,12 @@ function handleAddWordRequest() {
     let word = extractWord(text);
 
     if (word) {
-      admin.addWord(word)
-        .then(result => {
-          if (result == 23505) {
-            vk.sendMessage(`Я уже знаю слово "${ word }"! 😊`, 3000);
-          } else {
-            vk.sendMessage(`👍 Я запомнил слово "${ word }"!`, 3000);
-          }
-        });
+      const result = await admin.addWord(word);
+      if (result == 23505) {
+        vk.sendMessage(`Я уже знаю слово "${ word }"! 😊`, 3000);
+      } else {
+        vk.sendMessage(`👍 Я запомнил слово "${ word }"!`, 3000);
+      }
     } else {
       vk.getUserName(this.message.from_id)
         .then(name => vk.sendMessage(`${name}, я тебя не понимаю 😒`, 3000));
@@ -109,7 +97,7 @@ function handleAddWordRequest() {
   return false;
 }
 
-function handleDeleteWordRequest() {
+async function handleDeleteWordRequest() {
   let text = this.message.text.toLowerCase();
   let botMentioned = isBotMentioned(text);
   let isDeleteWordRequest = text.includes('забудь слово');
@@ -118,8 +106,8 @@ function handleDeleteWordRequest() {
     let word = extractWord(text);
     
     if (word) {
-      admin.deleteWord(word)
-        .then(() => vk.sendMessage(`👍 Я забыл слово "${ word }"!`, 3000));
+      await admin.deleteWord(word);
+      vk.sendMessage(`👍 Я забыл слово "${ word }"!`, 3000);
     } else {
       vk.getUserName(this.message.from_id)
         .then(name => vk.sendMessage(`${name}, я тебя не понимаю 😒`, 3000));
@@ -139,83 +127,67 @@ function extractWord(text) {
   return word === '' ? false : word;
 }
 
-function handleIdleState(resolve, reject) {
+async function handleIdleState(resolve, reject) {
   let text = this.message.text.toLowerCase();
   let category;
 
   if (isGameRequestMessage(text)) {
     resolve(true);
-    getRandomTask()
-      .then(task => {
-        answer = task.answer;
-        category = task.category;
-        return generatePhotos();
-      })
-      .then(result => {
-        console.log(answer);
-        setGameState({state: STATE_PLAYING, answer: answer});
-        
-        // TODO: больше приветственных сообщений
-        let welcomeMessages = [
-          `Игра начинается, отгадывать могут все! 😏 Какое слово я загадал?`,
-          `Я люблю играть! 😊 Я загадал слово, которое описывает эту картинку. Сможете угадать это слово?`,
+    const task = await getRandomTask();
+    answer = task.answer;
+    category = task.category;
+    try {
+      await generatePhotos();
+      console.log(answer);
+      await setGameState({state: STATE_PLAYING, answer: answer});
+      // TODO: больше приветственных сообщений
+      let welcomeMessages = [
+        `Игра начинается, отгадывать могут все! 😏 Какое слово я загадал?`,
+        `Я люблю играть! 😊 Я загадал слово, которое описывает эту картинку. Сможете угадать это слово?`,
+      ];
+      await vk.sendMessage(welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)], 3000);
+      let photoPath = __dirname + '/task.jpg';
+      await vk.sendPhoto(photoPath);
+      timeoutObj = setTimeout(giveHint, STEP_INTERVAL);
+    } catch (error) {
+      // Бот устал
+      if (error.message === 'usageLimits') {
+        // TODO: больше сообщений
+        let limitsMessages = [
+          `Что-то я устал играть... 😫 Приходите завтра 😊`,
+          `Давай продолжим завтра? Сегодня больше не хочется 😳`,
+          `Я уже наигрался, мне надо отдохнуть`,
         ];
 
-        return vk.sendMessage(welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)], 3000);
-      })
-      .then(response => {
-        timeoutObj = setTimeout(giveHint, STEP_INTERVAL);
-        let photoPath = __dirname + '/task.jpg';
-        return vk.sendPhoto(photoPath);
-      })
-      .catch(error => {
-        // Бот устал
-        if (error.message === 'usageLimits') {
-          // TODO: больше сообщений
-          let limitsMessages = [
-            `Что-то я устал играть... 😫 Приходите завтра 😊`,
-            `Давай продолжим завтра? Сегодня больше не хочется 😳`,
-            `Я уже наигрался, мне надо отдохнуть`,
-          ];
+        let limitsStickers = [13, 85, 2091, 5135, 5629];
 
-          let limitsStickers = [ 13, 85, 2091, 5135, 5629 ];
-
-          return vk.sendSticker(limitsStickers[Math.floor(Math.random() * limitsStickers.length)])
-            .then(response => {
-              return vk.sendMessage(limitsMessages[Math.floor(Math.random() * limitsMessages.length)], 5000);
-            });
-        };
-      });
+        await vk.sendSticker(limitsStickers[Math.floor(Math.random() * limitsStickers.length)]);
+        await vk.sendMessage(limitsMessages[Math.floor(Math.random() * limitsMessages.length)], 5000);
+      }
+    }
   } else {
     resolve(false);
   }
 }
 
-function generatePhotos() {
+async function generatePhotos() {
   let apiURL = 'https://www.googleapis.com/customsearch/v1';
   let key = process.env.GOOGLE_KEY;
   let cx = process.env.GOOGLE_SEARCH_ENGINE_ID;
   let start = randomInteger(1, 5);
-  let hintImgURL;
 
   let url = `${apiURL}?q=${encodeURIComponent(answer)}&cx=${cx}&fileType=jpg&num=2&safe=active&searchType=image&fields=items%2Flink&start=${start}&key=${key}`;
-  return needle('get', url)
-    .then(response => {
-      console.log(response.body);
-      if (response.body.error && response.body.error.errors[0].domain === 'usageLimits') {
-        throw new Error('usageLimits');
-      }
-      let taskImgURL = response.body.items[0].link;
-      hintImgURL = response.body.items[1].link;
-      return needle('get', taskImgURL);
-    })
-    .then(response => {
-      fs.writeFileSync(__dirname + '/task.jpg', response.body);
-      return needle('get', hintImgURL);
-    })
-    .then(response => {
-      fs.writeFileSync(__dirname + '/hint.jpg', response.body);
-    });
+  const googleResponse = await needle('get', url);
+  console.log(googleResponse.body);
+  if (googleResponse.body.error && googleResponse.body.error.errors[0].domain === 'usageLimits') {
+    throw new Error('usageLimits');
+  }
+  const taskImgURL = googleResponse.body.items[0].link;
+  const hintImgURL = googleResponse.body.items[1].link;
+  const taskImgResponse = await needle('get', taskImgURL);
+  const hintImgResponse = await needle('get', hintImgURL);
+  fs.writeFileSync(__dirname + '/task.jpg', taskImgResponse.body);
+  fs.writeFileSync(__dirname + '/hint.jpg', hintImgResponse.body);
 }
 
 function randomInteger(min, max) {
@@ -224,25 +196,24 @@ function randomInteger(min, max) {
   return rand;
 }
 
-function giveHint() {
-  timeoutObj = setTimeout(sendAnswer, STEP_INTERVAL);
-  
+async function giveHint() {
+
   // TODO: больше сообщений подсказок
   let hintMessages = [
     'Никто не знает? 😒 Вот подсказка!',
     'Я не думал, что будет так сложно... 😥 Держите подсказку',
   ];
 
-  photoPath = __dirname + '/hint.jpg';
+  const photoPath = __dirname + '/hint.jpg';
 
-  vk.sendMessage(hintMessages[Math.floor(Math.random() * hintMessages.length)])
-  .then(response => {
-    return vk.sendPhoto(photoPath);
-  });
+  await vk.sendMessage(hintMessages[Math.floor(Math.random() * hintMessages.length)]);
+  await vk.sendPhoto(photoPath);
+
+  timeoutObj = setTimeout(sendAnswer, STEP_INTERVAL);
 }
 
-function sendAnswer() {
-  setGameState({state: STATE_IDLE, answer: ''});
+async function sendAnswer() {
+  await setGameState({state: STATE_IDLE, answer: ''});
 
   // TODO: больше сообщений ответов
   let answerMessages = [
@@ -253,7 +224,7 @@ function sendAnswer() {
   vk.sendMessage(answerMessages[Math.floor(Math.random() * answerMessages.length)]);
 }
 
-function handlePlayingState(resolve, reject) {
+async function handlePlayingState(resolve, reject) {
   let text = this.message.text.toLowerCase();
 
   // Если игра уже идёт, но кто-то написал новый запрос на игру,
@@ -265,33 +236,30 @@ function handlePlayingState(resolve, reject) {
   }
 
   let answerIsCorrect = checkAnswer(text);
-  let triedToAddWord = false;
 
   if (answerIsCorrect) {
     clearTimeout(timeoutObj);
-    setGameState({state: STATE_IDLE, answer: ''});
-    vk.getUserName(this.message.from_id)
-      .then(function (name) {
-        let successMessages = [
-          `Браво, ${name}! Моё слово — ${answer} 👏`,
-          `${name}, ты умница! 😃 На картинке ${answer}`,
-          `Правильно, ${name}! 👍 Это действительно ${answer}`,
-          `И в этом раунде побеждает ${name}, разгадав слово "${answer}"! 😎`,
-          `Я увидел правильный ответ — ${answer}! ${name}, как тебе это удаётся? 🙀`,
-        ];
-        let successMessage = successMessages[Math.floor(Math.random() * successMessages.length)];
-        return vk.sendMessage(successMessage);
-      });
+    resolve(true);
+    await setGameState({state: STATE_IDLE, answer: ''});
+    const name = await vk.getUserName(this.message.from_id);
+    let successMessages = [
+      `Браво, ${name}! Моё слово — ${answer} 👏`,
+      `${name}, ты умница! 😃 На картинке ${answer}`,
+      `Правильно, ${name}! 👍 Это действительно ${answer}`,
+      `И в этом раунде побеждает ${name}, разгадав слово "${answer}"! 😎`,
+      `Я увидел правильный ответ — ${answer}! ${name}, как тебе это удаётся? 🙀`,
+    ];
+    let successMessage = successMessages[Math.floor(Math.random() * successMessages.length)];
+    vk.sendMessage(successMessage);
     // TODO: отправлять стикер
   } else {
     let word = extractWord(text);
     let botMentioned = isBotMentioned(text);
+    resolve(word && !botMentioned);
     if (word && !botMentioned) {
-      admin.addWord(word, false);
-      triedToAddWord = true;
+      await admin.addWord(word, false);
     }
   }
-  resolve(answerIsCorrect || triedToAddWord);
 }
 
 function isBotMentioned(text) {
@@ -313,7 +281,7 @@ function checkAnswer(entered) {
   return entered === answer.toLowerCase();
 }
 
-module.exports = function(message) {  
+module.exports = function(message) {
   this.message = message;
   return new Promise(handleMessage);
-}
+};
