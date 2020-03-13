a4a812d3f53015eec66526b1ac751dfc44cde009
Georgii Bagretsov <georgii.bagretsov@t-systems.com>
2020-03-13 12:58:51 +0300

    #24 Добавлены тесты для модуля chat

--- app/chat/chat.js
+++ app/chat/chat.js
@@ -1,9 +1,10 @@
 const vk = require('../vk');
+require('dotenv').config();
 
 module.exports = function(message) {  
   let text = message.text.toLowerCase();
 
-  if (text.startsWith('бот,') || text.includes('club171869330')) {
+  if (text.startsWith('бот,') || text.includes(`club${process.env.VK_GROUP_ID}`)) {
     let uid = message.from_id;
     vk.getUserName(uid)
       .then(function (response) {
