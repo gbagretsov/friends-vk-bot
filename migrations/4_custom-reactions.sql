be237c41a188e0163f26a2b35b0fccccc18888c2
Georgii Bagretsov <georgii.bagretsov@t-systems.com>
2020-06-22 13:27:01 +0300

    #45 Добавлена схема БД для пользовательских реакций

---
+++ migrations/4_custom-reactions.sql
@@ -0,0 +1,37 @@
+-- Up migration
+CREATE TABLE IF NOT EXISTS friends_vk_bot.custom_reactions
+(
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    probability SMALLINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS friends_vk_bot.stickers
+(
+    id INTEGER NOT NULL,
+    reaction_id INTEGER NOT NULL REFERENCES friends_vk_bot.custom_reactions(id) ON DELETE CASCADE,
+    PRIMARY KEY (id, reaction_id)
+);
+
+CREATE TABLE IF NOT EXISTS friends_vk_bot.phrases
+(
+    id SERIAL NOT NULL PRIMARY KEY,
+    reaction_id INTEGER NOT NULL REFERENCES friends_vk_bot.custom_reactions(id) ON DELETE CASCADE,
+    text TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS friends_vk_bot.responses
+(
+    id SERIAL NOT NULL PRIMARY KEY,
+    reaction_id INTEGER NOT NULL REFERENCES friends_vk_bot.custom_reactions(id) ON DELETE CASCADE,
+    type SMALLINT NOT NULL,
+    content TEXT NOT NULL
+);
+
+
+-- Down migration
+
+DROP TABLE IF EXISTS friends_vk_bot.stickers CASCADE;
+DROP TABLE IF EXISTS friends_vk_bot.phrases CASCADE;
+DROP TABLE IF EXISTS friends_vk_bot.responses CASCADE;
+DROP TABLE IF EXISTS friends_vk_bot.custom_reactions CASCADE;
