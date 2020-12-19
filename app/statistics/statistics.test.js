277c1e248d9226298470999f9fd339039299ad04
Georgii Bagretsov <georgii.bagretsov@t-systems.com>
2020-12-20 01:20:56 +0300

    # 73 Добавлены тесты для сбора статистики

--- app/statistics/statistics.test.js
+++ app/statistics/statistics.test.js
@@ -1,11 +1,11 @@
-const textMessage = { text: 'Hello', from_id: 1, conversation_message_id: 1 };
+const textMessage = { text: 'Hello', from_id: 1111, conversation_message_id: 1 };
 const stickerMessage = {
   attachments: [
     {
       type: 'sticker',
       sticker: { sticker_id: 1 }
     }],
-  from_id: 1,
+  from_id: 1111,
   conversation_message_id: 1
 };
 const audioMessage = {
@@ -14,7 +14,7 @@ const audioMessage = {
       type: 'audio_message',
       audio_message: { link_mp3: 'link_mp3' }
     }],
-  from_id: 1,
+  from_id: 2222,
   conversation_message_id: 1
 };
 const repostMessage = {
@@ -23,9 +23,10 @@ const repostMessage = {
       type: 'wall',
       wall: { id: 1 }
     }],
-  from_id: 1,
+  from_id: 1111,
   conversation_message_id: 1
 };
+const previouslyHandledMessage = { text: 'Hello', from_id: 1111, conversation_message_id: 9999 };
 
 beforeAll(() => {
 
@@ -37,16 +38,131 @@ afterEach(() => {
 });
 
 describe('Collect statistics', () => {
-  // TODO
-  // When bot receives a message and message has not been handled before, bot gathers info from this message
-  // When bot receives a message and message has been handled before, bot does not gather info from this message
-  // During gathering of info from message bot increases total messages counter if needed
-  // During gathering of info from message bot increases voice messages counter if needed
-  // During gathering of info from message bot increases stickers counter if needed
-  // During gathering of info from message bot increases reposts counter if needed
-  // During gathering of info from message bot increases messages counter for the current sender
+  test('When bot receives a message and message has not been handled before, bot gathers info from this message', done => {
+    setMocks();
+    const handleMessage = require('./statistics').handleMessage;
+    const db = require('../db');
+    handleMessage(textMessage);
+    setTimeout(() => {
+      expect(db.query).toHaveBeenCalledTimes(4);
+      done();
+    }, 500);
+  });
+
+  test('When bot receives a message and message has been handled before, bot does not gather info from this message', done => {
+    setMocks();
+    const handleMessage = require('./statistics').handleMessage;
+    const db = require('../db');
+    handleMessage(previouslyHandledMessage);
+    setTimeout(() => {
+      expect(db.query).toHaveBeenCalledTimes(1);
+      done();
+    }, 500);
+  });
+
+  test('During gathering of info from message bot increases total messages counter', done => {
+    setMocks();
+    const handleMessage = require('./statistics').handleMessage;
+    const db = require('../db');
+    handleMessage(textMessage);
+    setTimeout(() => {
+      expect(db.query.mock.calls[3][0]).toMatch(/SET value = 101 WHERE id = -1/);
+      done();
+    }, 500);
+  });
+
+  test('During gathering of info from message bot increases voice messages counter if needed', done => {
+    setMocks();
+    const handleMessage = require('./statistics').handleMessage;
+    const db = require('../db');
+    handleMessage(audioMessage);
+    setTimeout(() => {
+      expect(db.query.mock.calls[3][0]).toMatch(/SET value = 6 WHERE id = -2/);
+      done();
+    }, 500);
+  });
+
+  test('During gathering of info from message bot increases stickers counter if needed', done => {
+    setMocks();
+    const handleMessage = require('./statistics').handleMessage;
+    const db = require('../db');
+    handleMessage(stickerMessage);
+    setTimeout(() => {
+      expect(db.query.mock.calls[3][0]).toMatch(/SET value = 11 WHERE id = -3/);
+      done();
+    }, 500);
+  });
+
+  test('During gathering of info from message bot increases reposts counter if needed', done => {
+    setMocks();
+    const handleMessage = require('./statistics').handleMessage;
+    const db = require('../db');
+    handleMessage(repostMessage);
+    setTimeout(() => {
+      expect(db.query.mock.calls[3][0]).toMatch(/SET value = 21 WHERE id = -4/);
+      done();
+    }, 500);
+  });
+
+  test('During gathering of info from message bot does not increase voice messages counter if not needed', done => {
+    setMocks();
+    const handleMessage = require('./statistics').handleMessage;
+    const db = require('../db');
+    handleMessage(textMessage);
+    setTimeout(() => {
+      expect(db.query.mock.calls[3][0]).not.toMatch(/SET value = [0-9]* WHERE id = -2/);
+      done();
+    }, 500);
+  });
+
+  test('During gathering of info from message bot does not increase stickers counter if not needed', done => {
+    setMocks();
+    const handleMessage = require('./statistics').handleMessage;
+    const db = require('../db');
+    handleMessage(textMessage);
+    setTimeout(() => {
+      expect(db.query.mock.calls[3][0]).not.toMatch(/SET value = [0-9]* WHERE id = -3/);
+      done();
+    }, 500);
+  });
+
+  test('During gathering of info from message bot does not increase reposts counter if not needed', done => {
+    setMocks();
+    const handleMessage = require('./statistics').handleMessage;
+    const db = require('../db');
+    handleMessage(textMessage);
+    setTimeout(() => {
+      expect(db.query.mock.calls[3][0]).not.toMatch(/SET value = [0-9]* WHERE id = -4/);
+      done();
+    }, 500);
+  });
+
+  test('During gathering of info from message bot increases messages counter for the current sender ' +
+             '(case with the very first message of user)', done => {
+    setMocks();
+    const handleMessage = require('./statistics').handleMessage;
+    const db = require('../db');
+    handleMessage(textMessage);
+    setTimeout(() => {
+      expect(db.query.mock.calls[3][0]).toMatch(/INSERT INTO .* VALUES\(1111, 1\)/);
+      done();
+    }, 500);
+  });
+
+  test('During gathering of info from message bot increases messages counter for the current sender ' +
+             '(case with not first message of user)', done => {
+    setMocks();
+    const handleMessage = require('./statistics').handleMessage;
+    const db = require('../db');
+    handleMessage(audioMessage);
+    setTimeout(() => {
+      expect(db.query.mock.calls[3][0]).toMatch(/SET value = 26 WHERE id = 2222/);
+      done();
+    }, 500);
+  });
 
   test('Bot always passes message further to following handlers', () => {
+    setMocks();
     const handleMessage = require('./statistics').handleMessage;
     expect(handleMessage(textMessage)).toBe(false);
     expect(handleMessage(audioMessage)).toBe(false);
@@ -71,3 +187,27 @@ describe('Reset statistics', () => {
   // Bot correctly resets statistics for current month
   // Bot correctly sets statistics for previous month
 });
+
+function setMocks() {
+  jest.doMock('../db');
+  const db = require('../db');
+
+  const statistics = { rows: [
+      { id: -1, value: 100 },  // total
+      { id: -2, value: 5 }, // audio
+      { id: -3, value: 10 }, // stickers
+      { id: -4, value: 20 }, // reposts
+      { id: -5, value: 400 }, // prev month
+      { id: 2222, value: 25}, // amount for user 2222
+  ]};
+
+  db.query.mockImplementation(_query => {
+    if (_query.includes('WHERE conversation_message_id = 9999')) {
+      return Promise.resolve({ rows: [{ conversation_message_id: 9999 }] });
+    } else if (_query.includes('SELECT * FROM friends_vk_bot.statistics')) {
+      return Promise.resolve(statistics);
+    } else {
+      return Promise.resolve({ rows: []});
+    }
+  });
+}
