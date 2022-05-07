731197ab8131391edd505dba8f5fad7b9631572f
Georgii Bagretsov <gbagretsov@gmail.com>
2022-05-08 00:36:51 +0300

    #123 Исправлен путь до статических ресурсов

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import path from 'path';
 
 import receiver from './receiver/receiver';
 import {router as apiRouter} from './api/api';
@@ -8,7 +7,7 @@ import {router as apiRouter} from './api/api';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static('compiled/public'));
 app.use(bodyParser.json());
 
 receiver(app);
