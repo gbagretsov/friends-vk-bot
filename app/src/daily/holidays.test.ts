2bc4ce3af79aa338c4d206463f4fb5e9ea829580
Georgii Bagretsov <gbagretsov@gmail.com>
2022-05-07 17:09:50 +0300

    #129 Исправлены тесты Holidays

--- app/src/daily/holidays.test.ts
+++ app/src/daily/holidays.test.ts
@@ -1,53 +1,45 @@
-afterEach(() => {
-  jest.clearAllMocks();
-  jest.resetModules();
-});
+import holidaysModule from './holidays';
 
-async function createMocksForDate(year, month, day) {
-  const mockedDate = new Date(year, month - 1, day, 2);
-  Date.now = jest.spyOn(Date, 'now').mockImplementation(() => mockedDate.getTime());
+jest.useFakeTimers('modern');
+
+async function createMocksForDate(year: number, month: number, day: number): Promise<void> {
+  jest.setSystemTime(new Date(year, month - 1, day, 10));
 }
 
 test('Only New year is celebrated on 1st January 2020', async () => {
   await createMocksForDate(2020, 1, 1);
-  const { getHolidays } = require('./holidays');
-  const holidays = await getHolidays();
+  const holidays = await holidaysModule.getHolidays();
   expect(holidays).toHaveLength(1);
   expect(holidays).toContain('Новый год');
 });
 
 test('Two Russian, international and UN holidays are celebrated on 4th January 2020', async () => {
   await createMocksForDate(2020, 1, 4);
-  const { getHolidays } = require('./holidays');
-  const holidays = await getHolidays();
+  const holidays = await holidaysModule.getHolidays();
   expect(holidays).toHaveLength(2);
 });
 
 test('No Russian, international or UN holidays are celebrated on 30th January 2020', async () => {
   await createMocksForDate(2020, 1, 30);
-  const { getHolidays } = require('./holidays');
-  const holidays = await getHolidays();
+  const holidays = await holidaysModule.getHolidays();
   expect(holidays).toHaveLength(0);
 });
 
 test('Only Defender of the Fatherland Day is celebrated on 23rd February 2020', async () => {
   await createMocksForDate(2020, 2, 23);
-  const { getHolidays } = require('./holidays');
-  const holidays = await getHolidays();
+  const holidays = await holidaysModule.getHolidays();
   expect(holidays).toHaveLength(1);
   expect(holidays).toContain('День защитника Отечества в России');
 });
 
 test('Ten Russian, international and UN holidays are celebrated on 1st March 2020', async () => {
   await createMocksForDate(2020, 3, 1);
-  const { getHolidays } = require('./holidays');
-  const holidays = await getHolidays();
+  const holidays = await holidaysModule.getHolidays();
   expect(holidays).toHaveLength(10);
 });
 
 test('New year holiday is present in the list for 31st December 2020', async () => {
   await createMocksForDate(2020, 12, 31);
-  const { getHolidays } = require('./holidays');
-  const holidays = await getHolidays();
+  const holidays = await holidaysModule.getHolidays();
   expect(holidays).toContain('Новый год');
 });
