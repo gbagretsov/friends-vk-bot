fb190b725560875ed085ac6ae3dbaeb31183c7c8
Georgii Bagretsov <gbagretsov@gmail.com>
2022-05-22 17:34:42 +0300

    #133 Удалена поддержка демо-режима на фронтенде

--- front/src/token/Token.jsx
+++ front/src/token/Token.jsx
@@ -60,7 +60,7 @@ class Token extends Component{
       if (data.success) {
         this.setState({ pending: false });
         localStorage['t'] = this.state.token;
-        this.props.onAuthorized(this.state.token, data.demo);
+        this.props.onAuthorized(this.state.token);
       } else {
         throw new Error('token');
       }
