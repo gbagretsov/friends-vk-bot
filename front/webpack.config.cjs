4d54666b33d2d1392a0e98d7a6742727f73c350b
Georgii Bagretsov <gbagretsov@gmail.com>
2024-04-11 18:34:23 +0300

    Update canvas, update node, remove sass

--- front/webpack.config.cjs
+++ front/webpack.config.cjs
@@ -13,8 +13,8 @@ module.exports = {
         options: { presets: ['@babel/env'] }
       },
       {
-        test: /\.scss$/,
-        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+        test: /\.css$/,
+        use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
       }
     ]
   },
